110233922d61374cc69b4454111b40117dd261b8
colinguinane1 <colinguinane@gmail.com>
2024-11-05 21:23:45 -0700

    added loading state

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,54 @@
 "use client";
 import { useEffect, useState } from "react";
+import { Tab, TabGroup, TabList, TabPanel, TabPanels } from "@headlessui/react";
+import { PulseLoader } from "react-spinners";
 
 export default function Home() {
   const [data, setData] = useState("");
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     fetch(`${process.env.NEXT_PUBLIC_BACK_END_URL}/api/numbers/`)
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
         setData(data);
+        setLoading(false);
       });
   }, []);
+
+  if (loading) {
+    return (
+      <div className="grid min-h-screen min-w-screen place-content-center">
+        <PulseLoader className="animate-pulse" color={"white"} />
+      </div>
+    );
+  }
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       {data && (
         <>
           <h1 className="text-4xl font-bold text-center">
             Number Game Front-End
           </h1>
           <p>Fetched from {process.env.NEXT_PUBLIC_BACK_END_URL}</p>
-          <div className="grid grid-cols-1 gap-8 sm:grid-cols-2">
-            {Object.entries(data).map(([difficulty, value]) => (
-              <div
-                key={difficulty}
-                className="flex flex-col items-center p-8 border rounded-lg"
-              >
-                <h2 className="text-2xl font-semibold">{difficulty}</h2>
-                <p className="text-xl font-light">{value}</p>
-              </div>
-            ))}
-          </div>
+          <TabGroup>
+            <TabList className="flex items-center gap-4 capitalize">
+              {Object.entries(data).map(([key, value]) => (
+                <Tab
+                  className="rounded-full py-1 px-3 text-sm/6 font-semibold text-white focus:outline-none data-[selected]:bg-white/10 data-[hover]:bg-white/5 data-[selected]:data-[hover]:bg-white/10 data-[focus]:outline-1 data-[focus]:outline-white capitalize"
+                  key={key}
+                >
+                  {key}
+                </Tab>
+              ))}
+            </TabList>
+            <TabPanels>
+              <TabPanel>Content 1</TabPanel>
+              <TabPanel>Content 2</TabPanel>
+              <TabPanel>Content 3</TabPanel>
+            </TabPanels>
+          </TabGroup>
         </>
       )}
     </div>
