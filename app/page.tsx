ac1ef77b10e7059d3013518d237c5d12d6f0fa57
Colin Guinane <colinguinane@gmail.com>
2024-11-05 18:10:50 -0700

    dark mode update

--- app/page.tsx
+++ app/page.tsx
@@ -18,12 +18,12 @@ export default function Home() {
           <h1 className="text-4xl font-bold text-center">
             Number Game Front-End
           </h1>
-          <p>Fetched from {process.env.BACK_END_URL}</p>
+          <p>Fetched from {process.env.NEXT_PUBLIC_BACK_END_URL}</p>
           <div className="grid grid-cols-1 gap-8 sm:grid-cols-2">
             {Object.entries(data).map(([difficulty, value]) => (
               <div
                 key={difficulty}
-                className="flex flex-col items-center p-8 bg-gray-100 rounded-lg"
+                className="flex flex-col items-center p-8 border rounded-lg"
               >
                 <h2 className="text-2xl font-semibold">{difficulty}</h2>
                 <p className="text-xl font-light">{value}</p>
