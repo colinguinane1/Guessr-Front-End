247322f68a655dfa595f177bccca2e90298e7bff
colinguinane1 <colinguinane@gmail.com>
2024-11-29 14:39:22 -0700

    downgrade to react 18 for compatability .2

--- app/history/page.tsx
+++ app/history/page.tsx
@@ -14,7 +14,7 @@ export default function History() {
                 const data = await response.json();
                 setData(data);
             } catch (error) {
-                console.error("Error fetching data:", error);
+                console.error("Error fetching data:", error)
             }
         };
         fetchData();
