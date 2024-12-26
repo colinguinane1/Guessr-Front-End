c67cdaad68bdaca8cc6e801a1655a6a631c3914b
colinguinane1 <colinguinane@gmail.com>
2024-12-26 13:42:44 -0700

    Add title to Home component for improved user experience

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,11 @@ import Game from "@/components/game";
 
 export default function Home() {
   return (
-    <div className="grid place-content-center md:h-[80vh]  p-4 w-screen">
-      <Game />
+    <div>
+      <p className="absolute top-2 left-2 font-extrabold text-lg">Guessr</p>
+      <div className="grid place-content-center md:h-[80vh]  p-4 w-screen">
+        <Game />
+      </div>
     </div>
   );
 }
