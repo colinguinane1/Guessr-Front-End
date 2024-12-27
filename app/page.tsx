22f4902968f4bea010c07c4ffe848fd6392cefb1
colinguinane1 <colinguinane@gmail.com>
2024-12-26 21:39:31 -0700

    Adjust positioning and padding of title in Home component for improved layout

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ import Game from "@/components/game";
 export default function Home() {
   return (
     <div>
-      <p className="absolute top-2 left-2 font-extrabold text-lg">Guessr</p>
-      <div className="grid place-content-center md:h-[80vh]  p-4 w-screen">
+      <p className="absolute top-4 p-2 left-4 font-extrabold text-lg">Guessr</p>
+      <div className="grid place-content-center md:h-[80vh] h-[50vh]  p-4 w-screen">
         <Game />
       </div>
     </div>
